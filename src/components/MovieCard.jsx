27e4b2efe662a4aed85427eef5ffe159937bef37
Kosmysk <diego.urbano@estudiantes.uv.cl>
2025-07-10 14:41:06 -0400

    Cambio en la funcionalidad de las tarjetas de películas

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Star, Calendar } from 'lucide-react';
 import { tmdbService } from '../services/tmdb.js';
+
+import { navigate } from 'astro/virtual-modules/transitions-router.js';
 
 const MovieCard = ({ movie }) => {
   const imageUrl = tmdbService.getImageUrl(movie.poster_path);
   const releaseYear = movie.release_date ? new Date(movie.release_date).getFullYear() : 'N/A';
   const rating = movie.vote_average ? parseFloat(movie.vote_average).toFixed(1) : 'N/A';
 
+  const irPelicula = (url) => {
+    navigate(url)
+  }
+
   return (
-    <div className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 group">
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 group cursor-pointer" onClick={() => {
+      irPelicula(`/movie/${movie.id}`)
+    }}>
       <div className="relative overflow-hidden">
         <img
           src={imageUrl}
@@ -38,12 +46,12 @@ const MovieCard = ({ movie }) => {
           {movie.overview || 'Sin descripción disponible.'}
         </p>
         
-        <a
+{/*         <a
           href={`/movie/${movie.id}`}
           className="inline-block w-full text-center bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200"
         >
           Ver Detalles
-        </a>
+        </a> */}
       </div>
     </div>
   );
